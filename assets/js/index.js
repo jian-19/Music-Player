70f6d85f4397488bf712b3575372016bffe2fe90
jian-19 <jian-ferreira@hotmail.com>
2023-06-14 17:39:56 -0300

    new function button pause

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,21 @@
 const btnprevious = document.querySelector('.previous').addEventListener('click', previousMusic);
 const btnPlay = document.querySelector('.play').addEventListener('click', playMusic);
 const btnnext = document.querySelector('.next').addEventListener('click', nextMusic);
-
-
-
+const song = document.querySelector("audio");
 
 function previousMusic() {
-
+    console.log("clicado")
 }
+
 function playMusic() {
     const iconPlay = document.querySelector('.play')
-    if (iconPlay.value = "play_circle") {
+    if (iconPlay.innerText === "play_circle") {
         iconPlay.innerText = 'pause_circle';
-        iconPlay.value = 'pause_circle'
-    }
-    else if (iconPlay.value = "pause_circle") {
+    }else if (iconPlay.innerText === "pause_circle") {
         iconPlay.innerText = 'play_circle';
-        iconPlay.value = 'play_circle'
     }
 }
-function pauseMusic() {
-    if (iconPlay.value = "pause_circle") {
-        iconPlay.innerText = 'play_circle';
-        iconPlay.value = 'play_circle'
-    }
-}
+
 function nextMusic() {
     console.log("clicado")
 }
